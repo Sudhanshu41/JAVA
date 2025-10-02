24d68da6d14ab20be788f8c1d22136e1fd81ddc5
Sudhanshu <skhoney05@gmail.com>
2025-10-02 21:36:19 +0530

    Arithematic and shorthand

---
+++ lectures/Arithematic.java
@@ -0,0 +1,19 @@
+public class Arithematic {
+    public static void main(String[] args) {
+        int a=8;
+        int b=10;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+        double x=5;
+        double y=2;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+        System.out.println(x%y);
+    }
+}
