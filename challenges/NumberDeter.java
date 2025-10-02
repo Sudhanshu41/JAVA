7ec10ea0a6fdd6cc2d90c17e6d5f5228be1ec54f
Sudhanshu <skhoney05@gmail.com>
2025-10-03 01:50:53 +0530

    NumberDeter

--- challenges/NumberDeter.java
+++ challenges/NumberDeter.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import java.util.Scanner;
 
 public class NumberDeter {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter the number: ");
+        double num = input.nextDouble();
+        if (num>0){
+            System.out.println("Its a Positive Number!!");
+        } else if (num<0) {
+            System.out.println("Its a Negative Number!!");
+        }
+        else {
+            System.out.println("The number is ZERO!!!");
+        }
+    }
 }
