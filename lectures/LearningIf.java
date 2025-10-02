7788979b3fbcf3bf06985ac34cec541e2e9ca818
Sudhanshu <skhoney05@gmail.com>
2025-10-02 22:35:34 +0530

    IFhalf

---
+++ lectures/LearningIf.java
@@ -0,0 +1,14 @@
+public class LearningIf {
+    public static void main(String[] args) {
+        boolean isMale = true;
+        String name = "Sudhanshu";
+        System.out.println("before if");
+        if(isMale){
+            System.out.println("Mr."+name);
+        }
+        else {
+            System.out.println("Mrs."+name);
+        }
+        System.out.println("After if");
+    }
+}
