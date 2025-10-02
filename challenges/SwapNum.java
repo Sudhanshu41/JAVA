c4356ad9bd8d2f53d0093ed2f3b37d2f039e433c
Sudhanshu <skhoney05@gmail.com>
2025-10-02 17:06:47 +0530

    Swap and Assign

---
+++ challenges/SwapNum.java
@@ -0,0 +1,13 @@
+public class SwapNum {
+    public static void main(String[] args) {
+        int a = 9;
+        System.out.println("a:"+a);
+        int b=8;
+        System.out.println("b:"+b);
+        int c = b;
+        b = a;
+        System.out.println("b:"+b);
+        a=c;
+        System.out.println("a:"+a);
+    }
+}
--- lectures/Assignment.java
+++ lectures/Assignment.java
@@ -1,5 +1,8 @@
 public class Assignment {
     public static void main(String[] args) {
-
+            int myInt = 9;/* = is an assignment operator that takes value from right and store it in left*/
+        System.out.println(myInt);
+        int newInt = myInt;
+        System.out.println(newInt);
     }
 }
