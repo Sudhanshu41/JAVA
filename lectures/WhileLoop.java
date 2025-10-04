0302485360abbeba9d32499d0d262a580a6f7ad9
Sudhanshu <skhoney05@gmail.com>
2025-10-05 00:46:58 +0530

    While loop

--- lectures/WhileLoop.java
+++ lectures/WhileLoop.java
@@ -1,2 +1,9 @@
 public class WhileLoop {
+    public static void main(String[] args) {
+        int num=500; // initialization
+        while (num>=200){ //conditiom
+            System.out.println(num); // actual work
+            --num; //update
+        }
+    }
 }
