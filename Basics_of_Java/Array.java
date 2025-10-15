bd1c277dd7ae552c4114fe5e02d3d04b9719741a
Sudhanshu <skhoney05@gmail.com>
2025-10-15 22:14:23 +0530

    Commit

---
+++ Basics_of_Java/Array.java
@@ -0,0 +1,11 @@
+public class Array {
+    public static void main(String[] args) {
+        int arr[]= new int [10];
+        arr[0]=1;
+        arr[1]=2;
+        arr[2]=3;
+        for (int i=0;i<10;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
