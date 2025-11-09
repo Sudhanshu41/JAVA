0a7d8024f71788509b976656ffa5cb3b0087cb6a
Sudhanshu <skhoney05@gmail.com>
2025-11-09 20:44:56 +0530

    Basics Over

--- Challenges_part-1(Basics)/SumDiagonal.java
+++ Challenges_part-1(Basics)/SumDiagonal.java
@@ -2,18 +2,40 @@ public class SumDiagonal {
     public static void main(String[] args) {
         System.out.println("Welcome to Sum Diagonal Elements");
         int [][] numArray = ArrayUtility.input2DArray();
-        int DiagSum = DiagonalSum(numArray);
-        System.out.println("The Sum Of Diagonal Elements is " + DiagSum);
+        long sumleft = sumOfLeftDiagonal(numArray);
+        System.out.println("Sum of left diagonal is: "+sumleft);
+        long sumright = sumOfRightDiagonal(numArray);
+        System.out.println("Sum of right diagonal is: "+sumright);
+        long totalDiagonal = sumOfDiagonal(numArray);
+        System.out.println("Total diagonal is: "+totalDiagonal);
+
     }
-    public static int DiagonalSum (int [][] numArray){
-        int sum = 0;
-        for(int i=0;i<numArray.length;i++){
-            for(int j=0;j<numArray[i].length;j++) {
-                if (i == j) {
-                    sum += numArray[i][j];
-                }
-            }
+    public static long sumOfDiagonal(int [][] numArray) {
+        long leftSum = sumOfLeftDiagonal(numArray);
+        long rightSum = sumOfRightDiagonal(numArray);
+        long sum = leftSum + rightSum;
+        if(numArray.length %2 !=0){
+            int ind = numArray.length/2;
+            sum -= numArray[ind][ind];
+        }
+        return sum;
+    }
+    public static long sumOfLeftDiagonal(int [][] numArray) {
+        long sum = 0;
+        for (int i = 0; i < numArray.length; i++) {
+          sum += numArray[i][i];
+        }
+        return sum;
+    }
+    public static long sumOfRightDiagonal(int [][] numArray) {
+        long sum = 0;
+        for (int i = 0; i < numArray.length; i++) {
+            int col = numArray[i].length - i - 1;
+            sum += numArray[i][col];
+
         }
         return sum;
     }
 }
+
+
